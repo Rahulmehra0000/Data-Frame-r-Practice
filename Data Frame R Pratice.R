106827bafc18fb84135dd96f6222e9fc76c5dc7e
Rahul Mehra <83349780+Rahulmehra0000@users.noreply.github.com>
2021-05-05 17:18:50 +0530

    Add files via upload

---
+++ Data Frame R Pratice.R
@@ -0,0 +1,19 @@
+df<-data.frame(x=1:3, y=c("a","b","c"))
+df
+Df_new<-data.frame(height=c(150,160,149,256,235), weight=c(65,72,79,90,100))
+x
+df
+df[3,2]
+df[2:3,2]
+df[c(2,3),2]
+
+
+Df_new[3:5,]
+Df_new[c(3:5), c(1,2)]
+Df_new[2,2]
+Df_new[3,2]
+Df_new[4,]
+Df_new[4,1:2]
+Df_new[4,c(1,2)]
+Df_new[c(3:5),]
+Df_new[c(3,5),]
